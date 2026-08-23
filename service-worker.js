dc86ce781c30ed87499e11e20c1e2df1c5cf8e7c
Keyur Gohil <133516013+KeyurGohilUK@users.noreply.github.com>
2026-08-23 18:14:36 +0100

    Refresh cache for 1.14.1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'spv-property-calculator-v1.14.0-receipt-optimisation';
+const CACHE_NAME = 'spv-property-calculator-v1.14.1-receipt-size';
 const ROOT = new URL('./', self.location.href).href;
 const APP_SHELL = new URL('./index.html', self.location.href).href;
 const CONFIG_URL = new URL('./supabase-config.js', self.location.href).href;
