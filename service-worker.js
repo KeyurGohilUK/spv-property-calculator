a80e259fba3be3a9b1bc709f2cc90f400454d762
Keyur Gohil <133516013+KeyurGohilUK@users.noreply.github.com>
2026-08-23 15:14:24 +0100

    Refresh PWA cache for App Menu interaction

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'spv-property-calculator-v1.11.3-more-popup-layout';
+const CACHE_NAME = 'spv-property-calculator-v1.11.4-app-menu-dismiss';
 const ROOT = new URL('./', self.location.href).href;
 const APP_SHELL = new URL('./index.html', self.location.href).href;
 const CONFIG_URL = new URL('./supabase-config.js', self.location.href).href;
