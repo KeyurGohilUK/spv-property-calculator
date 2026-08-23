34ff37d8a1c02deec6abe127ae2b2a0101b700b8
Keyur Gohil <133516013+KeyurGohilUK@users.noreply.github.com>
2026-08-23 18:21:28 +0100

    Bump secondary page version to 1.14.2

--- secondary-page-header.js
+++ secondary-page-header.js
@@ -1,7 +1,7 @@
 import { getProperties, replaceProperties, getPendingDeletes, clearPendingDeletes } from './storage.js';
 import { getAllExpenses, replaceExpenses } from './expense-storage.js';
 
-const APP_VERSION = '1.14.1';
+const APP_VERSION = '1.14.2';
 const $ = (id) => document.getElementById(id);
 const header = document.querySelector('.header-inner');
 let cloudUser = null;
