3c22183d68fe61071a838acb5be5d21e4fdec732
Keyur Gohil <133516013+KeyurGohilUK@users.noreply.github.com>
2026-08-24 22:36:18 +0100

    Prevent property cards from stretching

--- install-component.js
+++ install-component.js
@@ -1,6 +1,6 @@
 import { isNewerVersion, setupUpdateNotifier } from './update-notifier.js';
 
-export const APP_VERSION = '1.21.4';
+export const APP_VERSION = '1.21.5';
 
 const APP_CACHE_PREFIX = 'spv-property-calculator-';
 const APP_UPDATE_ASSETS = Object.freeze([
