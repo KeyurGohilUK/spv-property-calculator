7fa58f859a92466db5fae53ccc19d3f81e62bd27
Keyur Gohil <133516013+KeyurGohilUK@users.noreply.github.com>
2026-09-16 21:53:04 +0100

    Update app version to 1.26.4

--- src/components/install-component.js
+++ src/components/install-component.js
@@ -1,7 +1,7 @@
 import { isNewerVersion, setupUpdateNotifier } from './update-notifier.js';
 import { setupDialog } from './dialog-helper.js';
 
-export const APP_VERSION = '1.26.3';
+export const APP_VERSION = '1.26.4';
 
 const APP_ASSET_MANIFEST = './app-assets.json';
 
